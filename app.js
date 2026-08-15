7b51f2cf66bd68cf63cd939a622f5d846c6d8d30
Gabriel Casagrande <gabriel.casamattos@hotmail.com>
2026-08-15 19:36:15 -0300

    Conectando com o banco de dados

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const db = require('./db/connection');
 
 const PORT = 3000;
 
@@ -8,6 +8,16 @@ app.listen(PORT, function () {
     console.log(`O Express está rodando na porta ${PORT}`);
 });
 
+// db connection
+db.authenticate()
+    .then(() => {
+        console.log('Conexão com o banco de dados realizada com sucesso!');     
+    })
+    .catch(err => {
+        console.error('Não foi possível conectar ao banco de dados:', err);
+    });
+    
+// routes
 app.get('/', (req, res) => {
     res.send('Está funcionando 3');
 });
